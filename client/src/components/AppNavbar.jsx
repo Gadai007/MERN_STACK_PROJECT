61bb5f7bf1227ffc90391217a05e49573a43ad6f
Your Name <you@example.com>
2020-11-01 16:11:15 +0530

    navbar added

---
+++ client/src/components/AppNavbar.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container
+} from 'reactstrap'
+
+const AppNavbar = () => {
+    const [ isOpen, setIsOpen ] = useState(false)
+
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
+    return(
+        <Navbar color='dark' dark expand='sm' className='mb-5'>
+        <Container>
+            <NavbarBrand href='/'>Shopping</NavbarBrand>
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
+                <Nav className='ml-auto' navbar>
+                    <NavItem>
+                        <NavLink href='https://github.com/Gadai007'>
+                            GitHub
+                    </NavLink>
+                    </NavItem>
+                </Nav>
+            </Collapse>
+        </Container>
+    </Navbar>
+    )   
+}
+
+export default AppNavbar
