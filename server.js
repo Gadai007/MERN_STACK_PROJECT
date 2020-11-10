5db35c0ecef0f2012b360806b5ee6aa8e02f0521
Your Name <you@example.com>
2020-11-10 16:48:58 +0530

    changes made

--- server.js
+++ server.js
@@ -32,14 +32,11 @@ app.use('/api/users', users)
 app.use('/api/auth', auth)
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
-  
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
+app.use(express.static(path.join(__dirname, '/build/')))
+
+app.use("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"))
+})
 
 //server
 
